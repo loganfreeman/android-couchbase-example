2b0f25304f3ee9468348c594b5aa32ef66083298
shanhong cheng <shnahongcheng@gmail.com>
2016-11-27 18:28:32 -0700

    initial commit

---
+++ app/src/main/java/com/raywenderlich/quizzdroid/QuestionActivity.java
@@ -0,0 +1,171 @@
+package com.raywenderlich.quizzdroid;
+
+/*
+ * Copyright (c) 2016 Razeware LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.couchbase.lite.Attachment;
+import com.couchbase.lite.CouchbaseLiteException;
+import com.couchbase.lite.Document;
+import com.couchbase.lite.LiveQuery;
+import com.couchbase.lite.Query;
+import com.couchbase.lite.QueryEnumerator;
+import com.couchbase.lite.QueryRow;
+import com.couchbase.lite.Revision;
+import com.couchbase.lite.support.LazyJsonArray;
+import com.raywenderlich.quizzdroid.adapter.QuestionOptionsAdapter;
+import com.raywenderlich.quizzdroid.model.Answer;
+import com.raywenderlich.quizzdroid.model.Question;
+
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+public class QuestionActivity extends AppCompatActivity {
+
+  private TextView mTextView;
+  private GridView mQuestionOptions;
+  private ImageView mImageQuestion;
+
+  private int mSelectedOption;
+  private Question mQuestion;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_question);
+
+    mTextView = (TextView) findViewById(R.id.question_view);
+    mImageQuestion = (ImageView) findViewById(R.id.imageQuestion);
+
+    Intent intent = getIntent();
+    String questionId = intent.getStringExtra(HomeActivity.EXTRA_INTENT_ID);
+
+// 2
+    DataManager manager = DataManager.getSharedInstance(getApplicationContext());
+    Document document = manager.database.getDocument(questionId);
+
+// 3
+    mQuestion = ModelHelper.modelForDocument(document, Question.class);
+    mTextView.setText(mQuestion.getText());
+
+    Query answers = Answer.getAnswersForQuestion(manager.database, mQuestion.get_id());
+    QueryEnumerator answersQuery = null;
+    try {
+      answersQuery = answers.run();
+    } catch (CouchbaseLiteException e) {
+      e.printStackTrace();
+    }
+
+    mQuestionOptions = (GridView) findViewById(R.id.question_options);
+    mQuestionOptions.setChoiceMode(AbsListView.CHOICE_MODE_SINGLE);
+    mQuestionOptions.setNumColumns(2);
+    mQuestionOptions.setSelector(R.drawable.selector_button);
+
+    mQuestionOptions.setAdapter(new QuestionOptionsAdapter(mQuestion.getOptions(),
+            getAnswerCounts(answersQuery)));
+
+    mQuestionOptions.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+      @Override
+      public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        mSelectedOption = position;
+      }
+    });
+
+    Revision revision = document.getCurrentRevision();
+    Attachment attachment = revision.getAttachment("image");
+    if (attachment != null) {
+      InputStream is = null;
+      try {
+        is = attachment.getContent();
+      } catch (CouchbaseLiteException e) {
+        e.printStackTrace();
+      }
+      Drawable drawable = Drawable.createFromStream(is, "image");
+      mImageQuestion.setImageDrawable(drawable);
+    }
+
+    showAnswers();
+
+  }
+
+  private Map<String,Integer> getAnswerCounts(QueryEnumerator answers) {
+
+    Map<String,Integer> answerCounts = new HashMap<String, Integer>();
+
+    for (String option: mQuestion.getOptions()) {
+      answerCounts.put(option, 0);
+    }
+
+    for (QueryRow row : answers) {
+      LazyJsonArray<Object> key = (LazyJsonArray<Object>) row.getKey();
+      String answer = (String) key.get(1);
+      answerCounts.put(answer, (Integer)row.getValue());
+    }
+
+    return answerCounts;
+  }
+
+  private void showAnswers() {
+    DataManager manager = DataManager.getSharedInstance(getApplicationContext());
+    Query answersQuery = Answer.getAnswersForQuestion(manager.database, mQuestion.get_id());
+    LiveQuery liveQuery = answersQuery.toLiveQuery();
+
+    liveQuery.addChangeListener(new LiveQuery.ChangeListener() {
+      @Override
+      public void changed(LiveQuery.ChangeEvent event) {
+        QueryEnumerator result = event.getRows();
+
+        Map<String,Integer> counts = getAnswerCounts(result);
+
+        final QuestionOptionsAdapter adapter =
+                (QuestionOptionsAdapter)mQuestionOptions.getAdapter();
+
+        adapter.setAnswerCounts(counts);
+
+        runOnUiThread(new Runnable() {
+          @Override
+          public void run() {
+            adapter.notifyDataSetChanged();
+          }
+        });
+      }
+    });
+    liveQuery.start();
+  }
+
+  public void onButtonClicked(View view) {
+    Answer answer = new Answer(mQuestion.get_id(), "answer",
+            mQuestion.getOptions().get(mSelectedOption));
+    ModelHelper.save(DataManager.getSharedInstance(getApplicationContext()).database, answer);
+  }
+}
---
+++ app/src/main/java/com/raywenderlich/quizzdroid/model/Answer.java
@@ -0,0 +1,99 @@
+package com.raywenderlich.quizzdroid.model;
+
+import com.couchbase.lite.Database;
+import com.couchbase.lite.Emitter;
+import com.couchbase.lite.Mapper;
+import com.couchbase.lite.Query;
+import com.couchbase.lite.Reducer;
+import com.couchbase.lite.View;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by scheng on 11/27/16.
+ */
+
+public class Answer {
+    private String _id;
+    private String _rev;
+    private String question_id;
+    private String type;
+    private String user_answer;
+
+    public Answer(String question_id, String type, String user_answer) {
+        this.question_id = question_id;
+        this.type = type;
+        this.user_answer = user_answer;
+    }
+
+    public static Query getAnswersForQuestion(Database database, String questionId) {
+        View view = database.getView("app/answers");
+        if (view.getMap() == null) {
+            view.setMapReduce(new Mapper() {
+                @Override
+                public void map(Map<String, Object> document, Emitter emitter) {
+                    if (document.get("type").equals("answer")) {
+                        List<Object> keys = new ArrayList<>();
+                        keys.add((String) document.get("question_id"));
+                        keys.add((String) document.get("user_answer"));
+                        emitter.emit(keys, null);
+                    }
+                }
+            }, new Reducer() {
+                @Override
+                public Object reduce(List<Object> keys, List<Object> values, boolean rereduce) {
+                    return values.size();
+                }
+            }, "1");
+        }
+        Query query = view.createQuery();
+        query.setGroupLevel(2);
+        query.setStartKey(Arrays.asList(questionId));
+        query.setEndKey(Arrays.asList(questionId, new HashMap<String, Object>()));
+        return query;
+    }
+
+    public String get_id() {
+        return _id;
+    }
+
+    public void set_id(String _id) {
+        this._id = _id;
+    }
+
+    public String get_rev() {
+        return _rev;
+    }
+
+    public void set_rev(String _rev) {
+        this._rev = _rev;
+    }
+
+    public String getQuestion_id() {
+        return question_id;
+    }
+
+    public void setQuestion_id(String question_id) {
+        this.question_id = question_id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getUser_answer() {
+        return user_answer;
+    }
+
+    public void setUser_answer(String user_answer) {
+        this.user_answer = user_answer;
+    }
+}
